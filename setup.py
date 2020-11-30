4a2e38d7df0ddad4329a775b9055e8f311608d01
Sylvan LE DEUNFF <sylvan.ledeunff@orange.com>
2020-11-30 09:17:18 +0100

    Split CI/CD workflow and use matrix tests in CD (#5)
    * split ci_cd into two differents jobs to save resources during push, pull_requests, but not on release.
    
    * Check using multiple python versions on delivery 3.5, 3.6, 3.7, 3.8, 3.9 with matrix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="music-browser",
     description="Music search engine for CrossPlay web service.",
-    version="0.0.2",
+    version="0.0.3",
     packages=setuptools.find_packages(),
     install_requires=["marshmallow", "requests"],
 )
