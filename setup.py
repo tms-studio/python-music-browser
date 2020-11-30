039cb002865a33282c02e3a1695512c284768965
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-30 13:02:28 +0100

    Hotfix release v0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Music search engine for CrossPlay web service.",
     author="Sylvan Le Deunff",
     author_email="sledeunf@gmail.com",
-    version="0.0.3",
+    version="0.0.4",
     url="https://github.com/tms-studio/python-music-browser",
     long_description=get_description_from_readme(),
     long_description_content_type="text/markdown",
@@ -19,13 +19,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=["marshmallow", "requests"],
     classifiers=[
-        "Development Status :: Beta",
         "Intended Audience :: Developers",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Topic :: Music :: Browse",
-        "Topic :: Music :: Metadata",
     ],
 )
