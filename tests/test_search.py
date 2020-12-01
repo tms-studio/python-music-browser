40a336d39db2e673a340fa4cb1e2a09acdaca32a
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-12-01 21:51:59 +0100

    add tests set for search samples

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,20 @@
+from samples.amazon_search import amazon_search
+from samples.deezer_search import deezer_search
 from samples.spotify_search import spotify_search
+from samples.browser_search import browser_search
+
+
+def test_amazon_search():
+    amazon_search("Lovers on the sun")
+
+
+def test_deezer_search():
+    deezer_search("Lovers on the sun")
 
 
 def test_spotify_search():
     spotify_search("Lovers on the sun")
+
+
+def test_browser_search():
+    browser_search("Lovers on the sun")
